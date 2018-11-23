45b043161794e9b655e01928f57ea935d8bcaec1
kirilzilla <kirilm@me.com>
2018-11-23 09:37:29 +0100

    Update run_spacy_flask.py

--- run_spacy_flask.py
+++ run_spacy_flask.py
@@ -1,76 +1,66 @@
 #!/usr/bin/python3
-import json
-
+from flask import Flask, Response, request
 import spacy
-from flask import Flask, render_template, request, jsonify
+import json
 
 app = Flask(__name__)
 
-class NLPToolkit:
-    nlp = None
-
-    def __init__(self, lang="en"):
-        self.nlp = spacy.load(lang)
-
-    def process(self, text):
-        return self.nlp(u"%s" % text)
-
-    def tokenize_text(self, text):
-        found_tokens = []
-        tokens = self.process(text)
-        for token in tokens:
-            found_tokens.append(
-                {
-                    "text": token.text, # Original word text
-                    "lemma": token.lemma_, # The Lemmatized text
-                    "pos": token.pos_, # Simple part-of-speech tag
-                    "tag": token.tag_, # Detailed part-of-speech tag
-                    "dep": token.dep_, # Syntactic dependency, i.e.: relation between tokens
-                    "shape": token.shape_, # Word shape
-                    "alpha": token.is_alpha, # Is a alpha character
-                    "stop": token.is_stop, # Is a stopword
-                    "head_text": token.head.text if token.head else None, # The head text
-                    "head_pos": token.head.pos_ if token.head else None, # The head part-of-speech tag
-                    #"childs": [child for child in token.children] # Dependants
-                }
-            )
-        return found_tokens
-
-nlp_de = NLPToolkit('de')
-nlp_en = NLPToolkit('en')
-
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-@app.route('/en')
-def home_en():
-    words = request.args.get('text', default=None)
-    tokens = nlp_en.tokenize_text(words)
-    return render_template('search.html', language="en", text=words, tokens=tokens)
-
-@app.route('/de', methods=['GET', 'POST'])
-def home_de_json():
-    words = request.args.get('text', default=None)
-    tokens = nlp_de.tokenize_text(words)
-    return render_template('search.html', language="de", text=words, tokens=tokens)
-
-@app.route('/json/en')
-def home_en_json():
-    words = request.args.get('text', default=None)
-    tokens = nlp_en.tokenize_text(words)
-    return jsonify(tokens)
-
-@app.route('/json/de', methods=['GET', 'POST'])
-def home_de():
-    words = request.args.get('text', default=None)
-    tokens = nlp_de.tokenize_text(words)
-    return jsonify(tokens)
+@app.route("/de", methods=['DE'])
+def de():
+    text = request.form.get('text')
+    filterType =  request.form.get('type')
+    #if not text or text == '':
+    #    return Response(json.dumps({ "sucess": False, "messages": ["No text submitted!"]}), mimetype='application/json')
+    #if filterType and not (filterType.upper() in ['PROPN', 'NOUN', 'VERB', 'ADP']):
+    #    return Response(json.dumps({ "sucess": False, "messages": ["Invalid type!"]}), mimetype='application/json')
+    # load as german
+    nlp = spacy.load('de')
+    doc = nlp(request.form['text'])
+    entities = []
+    for token in doc:
+        if not filterType or (filterType and token.pos_ == filterType.upper()):
+            entities.append({
+                "text": token.text,
+                "lemma": token.lemma_,
+                "type": token.pos_,
+                'tag': token.tag_,
+                'pos': token.pos_,
+                'start': token.start_char,
+                'stop': token.end_char
+            })
+    resp = {
+       "success": True,
+       "data": entities
+    }
+    return Response(json.dumps(resp), mimetype='application/json', headers={"Access-Control-Allow-Origin": "*"})
 
 
-if __name__ == '__main__':
-    app.run(
-        host="0.0.0.0",
-        port=3000,
-        debug=True
-    )
+@app.route("/en", methods=['POST'])
+def en():
+    text = request.form.get('text')
+    filterType =  request.form.get('type')
+    if not text or text == '':
+        return Response(json.dumps({ "sucess": False, "messages": ["No text submitted!"]}), mimetype='application/json')
+    if filterType and not (filterType.upper() in ['PROPN', 'NOUN', 'VERB', 'ADP']):
+        return Response(json.dumps({ "sucess": False, "messages": ["Invalid type!"]}), mimetype='application/json')
+    # load as english
+    nlp = spacy.load('en')
+    doc = nlp(text)
+    entities = []
+    for token in doc:
+        # if token.pos_=='NOUN':
+        if not filterType or (filterType and token.pos_ == filterType.upper()):
+            entities.append({
+                "text": token.text,
+                "lemma": token.lemma_,
+                "type": token.pos_,
+                'tag': token.tag_,
+                'pos': token.pos_,
+                'start': token.start_char,
+                'stop': token.end_char
+            })
+    resp = {
+       "success": True,
+       "data": entities
+    }
+    return Response(json.dumps(resp), mimetype='application/json', headers={"Access-Control-Allow-Origin": "*"})
