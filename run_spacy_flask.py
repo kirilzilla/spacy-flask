b2ac14c61a9f717819df51de148e9cfb562d1905
kirilzilla <kirilm@me.com>
2018-11-26 00:17:16 +0100

    Update run_spacy_flask.py

--- run_spacy_flask.py
+++ run_spacy_flask.py
@@ -5,17 +5,18 @@ import json
 
 app = Flask(__name__)
 
-@app.route("/de", methods=['DE'])
+@app.route("/de", methods=['POST'])
 def de():
     text = request.form.get('text')
     filterType =  request.form.get('type')
-    #if not text or text == '':
-    #    return Response(json.dumps({ "sucess": False, "messages": ["No text submitted!"]}), mimetype='application/json')
-    #if filterType and not (filterType.upper() in ['PROPN', 'NOUN', 'VERB', 'ADP']):
-    #    return Response(json.dumps({ "sucess": False, "messages": ["Invalid type!"]}), mimetype='application/json')
+    user_def_tags = request.form.get('user_def_tags')
+    if not text or text == '':
+        return Response(json.dumps({ "sucess": False, "messages": ["No text submitted!"]}), mimetype='application/json')
+    if filterType and not (filterType.upper() in ['PROPN', 'NOUN', 'VERB', 'ADP']):
+        return Response(json.dumps({ "sucess": False, "messages": ["Invalid type!"]}), mimetype='application/json')
     # load as german
     nlp = spacy.load('de')
-    doc = nlp(request.form['text'])
+    doc = nlp(text)
     entities = []
     for token in doc:
         if not filterType or (filterType and token.pos_ == filterType.upper()):
@@ -24,13 +25,15 @@ def de():
                 "lemma": token.lemma_,
                 "type": token.pos_,
                 'tag': token.tag_,
-                'pos': token.pos_,
-                'start': token.start_char,
-                'stop': token.end_char
+                'pos': token.pos_
+                #'label': token.label,
+                #'start': token.start_char,
+                #'stop': token.end_char
             })
     resp = {
        "success": True,
-       "data": entities
+       "data": entities,
+       "user_def_tags": user_def_tags
     }
     return Response(json.dumps(resp), mimetype='application/json', headers={"Access-Control-Allow-Origin": "*"})
 
@@ -39,28 +42,30 @@ def de():
 def en():
     text = request.form.get('text')
     filterType =  request.form.get('type')
+    user_def_tags = request.form.get('user_def_tags')
     if not text or text == '':
         return Response(json.dumps({ "sucess": False, "messages": ["No text submitted!"]}), mimetype='application/json')
     if filterType and not (filterType.upper() in ['PROPN', 'NOUN', 'VERB', 'ADP']):
         return Response(json.dumps({ "sucess": False, "messages": ["Invalid type!"]}), mimetype='application/json')
     # load as english
-    nlp = spacy.load('en')
+    nlp = en_core_web_sm.load('')
     doc = nlp(text)
     entities = []
     for token in doc:
-        # if token.pos_=='NOUN':
         if not filterType or (filterType and token.pos_ == filterType.upper()):
             entities.append({
                 "text": token.text,
                 "lemma": token.lemma_,
                 "type": token.pos_,
                 'tag': token.tag_,
-                'pos': token.pos_,
-                'start': token.start_char,
-                'stop': token.end_char
+                'pos': token.pos_
+                #'label': token.label,
+                #'start': token.start_char,
+                #'stop': token.end_char
             })
     resp = {
        "success": True,
-       "data": entities
+       "data": entities,
+       "user_def_tags": user_def_tags
     }
     return Response(json.dumps(resp), mimetype='application/json', headers={"Access-Control-Allow-Origin": "*"})
